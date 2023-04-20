63e4ade27938042baf1bc70e873870b7ffbca95b
gaobaishun <gaobaishun@foxmail.com>
2023-04-20 15:25:06 +0800

    首次提交

---
+++ src/test/java/com/gbs/staging/StagingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gbs.staging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StagingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
